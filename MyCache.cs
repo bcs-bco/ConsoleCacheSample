0887fcd436b8ca7616dca9d395058fba171b0233
Glenn Chen <glenn.chen@broadwaytw.onmicrosoft.com>
2020-06-11 17:15:16 +0800

    加入專案檔案。

---
+++ MyCache.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCacheSample
+{
+    //Singleton Pattern
+    public class MyCache
+    {
+        private static ObjectCache _cache = null;
+        static readonly object locker = new object();
+
+        //public static readonly CacheEntryUpdateCallback MyCacheUpdateCallback;
+
+        private MyCache() { }
+        
+
+        public static ObjectCache CacheInstance
+        {
+            get {
+
+                //if (_cache == null)
+                //    _cache = MemoryCache.Default;
+
+                //Thread Safe寫法
+                if (_cache == null)
+                    lock (locker)
+                        if (_cache == null)
+                            _cache = MemoryCache.Default;
+                return _cache;
+            }
+        }
+
+        //千萬不要這樣做....
+        //static CacheItemPolicy _defaultPolicy = new CacheItemPolicy();
+        //public static CacheItemPolicy DefaultPolicy
+        //{
+        //    get
+        //    {
+        //        _defaultPolicy.AbsoluteExpiration = DateTime.Now.AddSeconds(30d);
+        //        return _defaultPolicy;
+        //    }
+        //}
+
+        public static CacheItemPolicy DefaultPolicy
+        {
+            get
+            {
+                CacheItemPolicy _defaultPolicy = new CacheItemPolicy();
+                _defaultPolicy.AbsoluteExpiration = DateTime.Now.AddSeconds(30d);
+                return _defaultPolicy;
+            }
+        }
+
+        static readonly Dictionary<string, CacheItemPolicy> Policys = new Dictionary<string, CacheItemPolicy>();
+        public static void AddToPolicyStore(string key, CacheItemPolicy cacheItemPolicy)
+        {
+            Policys.Add(key, cacheItemPolicy);
+        }
+
+        public static void GetPolicyFromStore(string Key)
+        {
+            return Policys[key];
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleCacheSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ObjectCache cache = MemoryCache.Default;
+            //CacheItemPolicy policy = new CacheItemPolicy();
+            //policy.AbsoluteExpiration = DateTime.Now.AddSeconds(10d);
+            //policy.SlidingExpiration = TimeSpan.FromSeconds(3);
+
+            //cache["glenn"] = "ABCDFG";
+            //cache.Set("glenn", "glenn.chen", policy);
+            //bool isAdd = cache.Add("glenn", "aries.lu", policy);
+            //Console.WriteLine($"IsAdd? {isAdd.ToString()}");
+            //while (true)
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine($"({DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")})\tCache value:\t{cache["glenn"]}");
+
+            //    if (!cache.Contains("glenn")) break;
+
+            //}
+            //===========================================================
+            var cache = MyCache.CacheInstance;
+            var policy = MyCache.DefaultPolicy;
+            //MyCache.DefaultPolicy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTime.Now.AddSeconds(30d);
+            var books = WriteFileCache(policy);
+            cache.Set(books, policy);
+
+            //這兩個只能保留一個
+            policy.UpdateCallback = (x) => { cache.Remove("books"); Console.WriteLine("===books update==="); };
+            //policy.RemovedCallback = (x) => Console.WriteLine("===books removed===");
+
+            while (true)
+            {
+                var mybooks = (List<XElement>)cache["books"];
+                foreach (var book in mybooks)
+                {
+                    Console.WriteLine($"{book.Attribute("id").Value}\t{book.Element("author").Value}\t{book.Element("name").Value}");
+                }
+                Thread.Sleep(1000);
+                if (!cache.Contains("books")) break;
+            }
+
+
+            Console.WriteLine("Done!!!");
+            Console.ReadKey();
+        }
+
+        static CacheItem WriteFileCache(CacheItemPolicy policy)
+        {
+            string rootPath = AppDomain.CurrentDomain.BaseDirectory;
+            string dataPath = Path.Combine(rootPath, "Data", "data.xml");
+            var books = XDocument.Load(dataPath);
+
+            //實體檔案異動時回收快取
+            policy.ChangeMonitors.Add(new HostFileChangeMonitor(new List<string>() { dataPath }));
+
+            //DB檔案異動時回收快取
+            //policy.ChangeMonitors.Add(new SqlChangeMonitor(null));
+
+            //繼承實作 ChangeMonitor 類別來建立獨有的監控邏輯
+            //policy.ChangeMonitors.Add(new MyChangeMonitor());
+
+            return new CacheItem("books", books.Root.Elements().ToList());
+
+        }
+
+
+    }
+
+    public class MyChangeMonitor : ChangeMonitor
+    {
+        public override string UniqueId => throw new NotImplementedException();
+
+        protected override void Dispose(bool disposing)
+        {
+           
+        }
+    }
+}
